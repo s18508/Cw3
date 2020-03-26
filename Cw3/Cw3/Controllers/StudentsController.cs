fb90747aa6a86d2f976d6b38ec70df4c3e0d1f81
Dominika <Dominika@DESKTOP-4VPBBJI>
2020-03-26 17:19:37 +0100

    stare

---
+++ Cw3/Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw3.DAL;
+using Cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id)
+        {
+         
+            return Ok("Aktualizacja ukonczona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+
+            return Ok("Usuwanie ukonczone");
+        }
+
+
+
+    }
+}
